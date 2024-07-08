85e5109855829f7e940a0cd59c962a5ab9e5125e
jmeno1011 <jwhltn8282@gmail.com>
2024-07-09 01:59:55 +0900

    try tutorial

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Home from './Home.tsx'
+// import Home from './Home.tsx'
 import About from './About.tsx'
 import React from 'react'
+import Root from './routes/root.tsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />
+    // element: <Home />
+    element: <Root />
   },
   {
     path: "about",
