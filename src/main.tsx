464fbc81ff66fc2f68296af5f4ec6c2e13d11268
jmeno1011 <jwhltn8282@gmail.com>
2024-07-09 01:39:07 +0900

    edit main, we need strict-mode

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Home from './Home.tsx'
 import About from './About.tsx'
+import React from 'react'
 
 const router = createBrowserRouter([
   {
@@ -16,5 +17,7 @@ const router = createBrowserRouter([
 ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router} />
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
