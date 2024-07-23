e5d30252993ad8a8316842a7d1a4523c71aad71f
jmeno1011 <jwhltn8282@gmail.com>
2024-07-23 17:22:59 +0900

    edit vite.config.ts

--- src/pages/EnvironmentConfirm.tsx
+++ src/pages/EnvironmentConfirm.tsx
@@ -1,10 +1,13 @@
 export default function EnvironmentConfirm() {
   return (
-    <div className='flex flex-col'>
+    <div className="flex flex-col">
       <h1 className="font-bold">환경변수 확인</h1>
       <div className="flex gap-2 py-2">
-        <label className="font-semibold">모드:</label><span className="font-semibold text-indigo-700">{import.meta.env.MODE}</span>
+        <label className="font-semibold">모드:</label>
+        <span className="font-semibold text-indigo-700">
+          {import.meta.env.MODE}
+        </span>
       </div>
     </div>
-  )
+  );
 }
